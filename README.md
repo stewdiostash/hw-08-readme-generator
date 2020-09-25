a033a6386064ee171b1b35e92fb56f52204e3161
Andrew Stewart <1andrewstewart@gmail.com>
2020-09-24 20:08:16 -0400

    updated questions section

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# Big one
+# Readme generator
+## Description
+        
+Automates readme via inquirer
+![MIT-license](https://img.shields.io/badge/license-MIT-blue)
+
+
+## Table of Contents
+
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+
+## Installation
+
+None to speak of
+
+
+## Usage
+
+None
+
+
+## License
+
+MIT
+Copyright (c) 2020 Andrew Stewart
+
+
+## Contributing
+
+Just call me
+
+
+## Tests
+
+Bang on your laptop
+
+
+## Questions
+
+Don't ask
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,55 +1,55 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## Description
+        
+${data.description}
+
+![${data.license}-license](https://img.shields.io/badge/license-${data.license}-blue)
 
 
-          ## Description
-                  
-          ${data.description}
-          ![${data.license}-license](https://img.shields.io/badge/license-${data.license}-blue)
+## Table of Contents
+
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
 
 
-          ## Table of Contents
-          
-          * [Description](#description)
-          * [Installation](#installation)
-          * [Usage](#usage)
-          * [License](#license)
-          * [Contributing](#contributing)
-          * [Tests](#tests)
-          * [Questions](#questions)
-          
-          
-          ## Installation
-          
-          ${data.installation}
-          
-          
-          ## Usage
-          
-          ${data.usage}
-          
-          
-          ## License
+## Installation
 
-          ${data.license}
-          Copyright (c) 2020 ${data.author}
-          
-          
-          ## Contributing
-          
-          ${data.contributing}
-          
-          
-          ## Tests
-          
-          ${data.tests}
-          
-          
-          ## Questions
-          
-          ${data.questions}
-  
+${data.installation}
+
+
+## Usage
+
+${data.usage}
+
+
+## License
+
+${data.license}
+Copyright (c) 2020 ${data.author}
+
+
+## Contributing
+
+${data.contributing}
+
+
+## Tests
+
+${data.tests}
+
+
+## Questions
+https://github.com/${data.github}
+
+${data.email}
+
 `
 
 }
